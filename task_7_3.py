224a808cfb009d5c1a204152bd2dbfc889424b49
piunovds <90773282+piunovds@users.noreply.github.com>
2022-05-31 18:14:54 +0300

    Add files via upload

---
+++ task_7_3.py
@@ -0,0 +1,27 @@
+'''
+3. Создать структуру файлов и папок, как написано в задании 2 (при помощи скрипта или «руками» в проводнике).
+Написать скрипт, который собирает все шаблоны в одну папку templates, например:
+|--my_project
+   ...
+  |--templates
+   |   |--mainapp
+   |   |  |--base.html
+   |   |  |--index.html
+   |   |--authapp
+   |      |--base.html
+   |      |--index.html
+
+Примечание: исходные файлы необходимо оставить; обратите внимание, что html-файлы расположены в родительских папках
+(они играют роль пространств имён); предусмотреть возможные исключительные ситуации; это реальная задача,
+которая решена, например, во фреймворке django.
+'''
+import os
+import shutil
+
+folder = 'my_project'
+for root, dirs, files in os.walk(folder):
+    for item in dirs:
+        if item == 'templates':
+           shutil.copytree(os.path.join(root, item),
+                           os.path.join(folder, 'templates'),
+                           dirs_exist_ok=True)
